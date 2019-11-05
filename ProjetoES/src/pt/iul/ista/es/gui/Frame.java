1b5c4d7f360d30e36707b639611de12ff25d73d1
jrlmc-iscteiul <jrlmc@iscte-iul.pt>
2019-11-05 09:56:49 +0000

    maven poi e classes todas a funcionar

--- ProjetoES/src/pt/iul/ista/es/gui/Frame.java
+++ ProjetoES/src/pt/iul/ista/es/gui/Frame.java
@@ -1,3 +1,4 @@
+package pt.iul.ista.es.gui;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
--- ProjetoES/src/pt/iul/ista/es/gui/GUI.java
+++
@@ -1,5 +0,0 @@
-package pt.iul.ista.es.gui;
-
-public class GUI {
-
-}
