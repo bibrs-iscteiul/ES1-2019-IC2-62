8bff02c63b08a962757f9fe1fa834936bd119bbd
jrlmc-iscteiul <jrlmc@iscte-iul.pt>
2019-12-02 11:52:43 +0000

    JUnit Maven e classes criadas

---
+++ ProjetoES/src/pt/iul/ista/es/tests/ErrorDetectionTest.java
@@ -0,0 +1,29 @@
+package pt.iul.ista.es.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ErrorDetectionTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
